1cf45f0ee4220a26eec7b5b9c4f8a9157cecaca8
Kleber Correia <kleber@git.com>
2021-06-07 15:56:31 +0100

    Handle changing page size event

--- lib/nfl_rushing_web/live/page_live.ex
+++ lib/nfl_rushing_web/live/page_live.ex
@@ -5,6 +5,17 @@ defmodule NflRushingWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", players: NFLStats.paginated_players)}
+    {:ok, assign(socket, query: "", per_page: 10, players: NFLStats.paginated_players())}
+  end
+
+  @impl true
+  def handle_event("update_players", %{"pagination" => %{"per_page" => per_page}}, socket) do
+    {:noreply, assign(socket, players: NFLStats.paginated_players(page_size: per_page))}
+  end
+
+  def options_for_select_with_selected(options, selected) do
+    Enum.map(options, fn option ->
+      [key: option, value: option, selected: option == selected]
+    end)
   end
 end
